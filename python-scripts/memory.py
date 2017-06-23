fe07753a1014c3f385537e2c5d6b0662b78b8358
Jannik Vogel <email@jannikvogel.de>
2017-06-23 17:48:07 +0200

    HACK: Identity map physical pages

--- python-scripts/memory.py
+++ python-scripts/memory.py
@@ -31,17 +31,27 @@ def map_page(virtual_address, mapped):
 	pde_addr = pde_base + (virtual_address >> 22) * 4
 	#print("PDE at 0x" + format(pde_addr, '08X'))
 	pde = read_u32(pde_addr) # Get PDE entry
-	if False: # FIXME: Only if not valid
+	#print("PDE mapping to 0x" + format(pde & 0xFFFFF000, '08X'))
+	if (pde & 1) == 0:
+		print("PDE not valid?!")
 		return
-	if True: # FIXME: Only if not large pages
-		pte_base = 0xC0000000 #FIXME: Why not pde & 0xFFFFF000
+	if (pde & (1 << 7)) == 0: # Only if not large pages (0 = 4kiB, 1 = 4MiB)
+		pte_base = 0xC0000000 # Hardcoded PTE
 		pte_addr = pte_base + (virtual_address >> 12) * 4 # FIXME: `& 0x3FF` ?
 		#print("PTE at 0x" + format(pte_addr, '08X'))
 		pte = read_u32(pte_addr) # Get PTE entry
 		was_mapped = (pte & 1 == 1)
+		#if (was_mapped) == 0:
+		#	print("PDE was not valid?!")
 		if mapped:
 			pte |= 0x00000001
 		else:
 			pte &= 0xFFFFFFFE
-	write_u32(pte_addr, pte)
+
+		# Hack to identity map physical pages
+		if (virtual_address & 0x80000000) != 0:
+			pte = (virtual_address & 0x7FFFF000) | (pte & 0xFFF)
+
+		#print("PTE mapping to 0x" + format(pte & 0xFFFFF000, '08X'))
+		write_u32(pte_addr, pte)
 	return was_mapped
