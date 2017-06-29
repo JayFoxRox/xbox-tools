75c89aa0066ea03cf4254a5b5dd01efdc67392d9
Jannik Vogel <email@jannikvogel.de>
2017-06-29 18:52:49 +0200

    Tabs vs. spaces solved

--- python-scripts/xbox/interface/if_gdb.py
+++ python-scripts/xbox/interface/if_gdb.py
@@ -4,20 +4,20 @@ import gdb
 inf = gdb.selected_inferior()
 
 def read(address, size):
-	return bytes(inf.read_memory(address, size))
+  return bytes(inf.read_memory(address, size))
 
 def write(address, data):
-	value = bytes(data)
-	inf.write_memory (address, value)
+  value = bytes(data)
+  inf.write_memory (address, value)
 
 def call(address, stack, registers=None):
   assert(False)
   # Some magic with `call` using:
   # gdb.execute (command [, from_tty [, to_string]])
   # Maybe?
-	out_registers = {}
-	out_registers['eax'] = 0
-	return out_registers
+  out_registers = {}
+  out_registers['eax'] = 0
+  return out_registers
 
 interface.read = read
 interface.write = write
