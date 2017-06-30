c4aeaa89d254f4e98dc7835e375f5b65e30fbfc3
Jannik Vogel <email@jannikvogel.de>
2017-06-30 19:19:29 +0200

    Move some APU code around

---
+++ python-scripts/inspect_apu_vp.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+from xbox import *
+
+#FIXME: Move to fields, however, globals() has another context there >.<
+def Field(offset_name, mask_name=None):
+  offset_value = globals().get(offset_name)
+  if offset_value == None:
+    raise NameError(offset_name)
+  if mask_name == None:
+    mask_value = 0xFFFFFFFF
+  else:
+    mask_name = offset_name + '_' + mask_name
+    mask_value = globals().get(mask_name)
+    if mask_value == None:
+      raise NameError
+    mask_value = GetMask(mask_value)
+  return(offset_value, mask_value)
+
+def ReadVoiceField(voice, field_name, mask_name=None, comment=None, show=True):
+  real_field_name = 'NV_PAVS_VOICE_' + field_name
+  field = Field(real_field_name, mask_name)
+  value = apu.ReadVoice(voice, *field)
+  if show:
+    mask_length = (field[1] >> ffs(field[1])).bit_length()
+    if mask_length == 1:  #FIXME: If mask is 1 bit only, display bool
+      value_str = str(value > 0)
+    else:
+      #FIXME: Format by mask length
+      value_str = "0x" + format(value, 'X')
+    #FIXME: Use decimal if asked to do so
+    #FIXME: append comment if there is one
+    print(field_name + (("." + mask_name) if mask_name != None else "") + ": " + value_str)
+  return value
+
+
+def ProcessStream(voice):
+  ssla_count = ReadVoiceField(voice, 'CUR_PSL_START', 'SSLA_COUNT')
+  ssla_base = ReadVoiceField(voice, 'CUR_PSL_START', 'SSLA_BASE')
+  cso = ReadVoiceField(voice,'PAR_OFFSET', 'CSO')
+  sslb_count = ReadVoiceField(voice, 'PAR_NEXT', 'SSLB_COUNT')
+  sslb_base = ReadVoiceField(voice, 'PAR_NEXT', 'SSLB_BASE')
+
+  #FIXME: More research.. this might be wrong
+  #FIXME: Read internal memory to figure out where the SSL data is stored?
+  #FIXME: Read VPSSLADDR as it stores sample data for SSL?
+
+  pass
+
+last_list = None
+def ListVoices(voice, name):
+  global last_list
+  if name != last_list:
+    print('Starting ' + name + ' list')
+    last_list = name
+
+  is_stream = ReadVoiceField(voice, 'CFG_FMT', 'DATA_TYPE', show=False) > 0
+
+  # Enable this / modify this if you only want to show one type: buffers or streams
+  if False:
+    if not is_stream:
+      next_voice = ReadVoiceField(voice, 'TAR_PITCH_LINK', 'NEXT_VOICE_HANDLE', show=False)
+      return next_voice
+
+  print("")
+  print("Voice: 0x" + format(voice, '04X'))
+
+  vbin = ReadVoiceField(voice, 'CFG_VBIN', show=False)
+  # lots of unk stuff
+
+  #FIXME: Don't print
+  voice_fmt = ReadVoiceField(voice, 'CFG_FMT', show=False)
+
+  samples_per_block = ReadVoiceField(voice, 'CFG_FMT', 'SAMPLES_PER_BLOCK')
+
+  print("Data type: " + ("stream" if is_stream else "buffer"))
+
+
+  loop = ReadVoiceField(voice, 'CFG_FMT', 'LOOP') > 0
+
+  # (26,26) ???
+
+  is_stereo = ReadVoiceField(voice, 'CFG_FMT', 'STEREO') > 0
+
+  NV_PAVS_VOICE_CFG_FMT_SAMPLE_SIZE_values = [ 'U8', 'S16', 'S24', 'S32' ]
+  sample_size = ReadVoiceField(voice, 'CFG_FMT', 'SAMPLE_SIZE', show=False)
+  print("Sample size: " + NV_PAVS_VOICE_CFG_FMT_SAMPLE_SIZE_values[sample_size])
+
+  NV_PAVS_VOICE_CFG_FMT_CONTAINER_SIZE_values = ['B8', 'B16', 'ADPCM', 'B32']
+  container_size_values = [1, 2, 4, 4]
+  container_size = ReadVoiceField(voice, 'CFG_FMT', 'CONTAINER_SIZE', show=False)
+  print("Container size: " + NV_PAVS_VOICE_CFG_FMT_CONTAINER_SIZE_values[container_size])
+  # lots of unk stuff
+
+  paused = ReadVoiceField(voice, 'PAR_STATE', 'PAUSED')
+  active = ReadVoiceField(voice, 'PAR_STATE', 'ACTIVE_VOICE')
+
+  cur_vola = ReadVoiceField(voice, 'CUR_VOLA', show=False)
+  cur_volb = ReadVoiceField(voice, 'CUR_VOLB', show=False)
+  cur_volc = ReadVoiceField(voice, 'CUR_VOLC', show=False)
+
+  tar_vola = ReadVoiceField(voice, 'TAR_VOLA', show=False)
+  tar_volb = ReadVoiceField(voice, 'TAR_VOLB', show=False)
+  tar_volc = ReadVoiceField(voice, 'TAR_VOLC', show=False)
+
+  bins_data = []
+  bins_data.append(vbin & 0x1F) # 0
+  bins_data.append((vbin >> 5) & 0x1F) # 1
+  bins_data.append((vbin >> 10) & 0x1F) # 2
+  # (15,15) ?
+  bins_data.append((vbin >> 16) & 0x1F) # 3
+  bins_data.append((vbin >> 21) & 0x1F) # 4
+  bins_data.append((vbin >> 26) & 0x1F) # 5
+  # (31,31) ?
+  bins_data.append(voice_fmt & 0x1F) # 6
+  bins_data.append((voice_fmt >> 5) & 0x1F) # 7
+
+  def decode_volumes(a, b, c):
+    data = []
+    data.append((a >> 4) & 0xFFF) # 0
+    data.append((a >> 20) & 0xFFF) # 1
+    data.append((b >> 4) & 0xFFF) # 2
+    data.append((b >> 20) & 0xFFF) # 3
+    data.append((c >> 4) & 0xFFF) # 4
+    data.append((c >> 20) & 0xFFF) # 5
+    data.append(((c & 0xF) << 8) | ((b & 0xF) << 4) | (a & 0xF)) # 6
+    data.append(((c >> 8) & 0xF00) | ((b >> 12) & 0xF0) | ((a >> 16) & 0xF)) # 7
+    return data
+  cur_vols_data = decode_volumes(cur_vola, cur_volb, cur_volc)
+  tar_vols_data = decode_volumes(tar_vola, tar_volb, tar_volc)
+
+  #assert(False) #FIXME: Untested!
+
+  bins = ""
+  cur_vols = ""
+  tar_vols = ""
+  for i in range(0, 8):
+    bins += "  " + ("0x" + format(bins_data[i], 'X')).ljust(5)
+    cur_vols += "  0x" + format(cur_vols_data[i], '03X')
+    tar_vols += "  0x" + format(tar_vols_data[i], '03X')
+  print("VBIN   " + bins)
+  print("CUR_VOL" + cur_vols)
+  print("TAR_VOL" + tar_vols)
+
+  pitch = ReadVoiceField(voice, 'TAR_PITCH_LINK', 'PITCH')
+
+  freq = apu.PitchToFrequency(pitch)
+  print("Frequency: " + str(freq) + " Hz")
+
+  if is_stream:
+
+    ProcessStream(voice)
+
+  else:
+
+    psl_start_ba = ReadVoiceField(voice, 'CUR_PSL_START','BA')
+    cur_psh_sample = ReadVoiceField(voice, 'CUR_PSH_SAMPLE','LBO')
+    par_offset = ReadVoiceField(voice, 'PAR_OFFSET','CBO') # Warning: Upper 8 bits will be 0xFF (?) on hw!
+    ebo = ReadVoiceField(voice, 'PAR_NEXT','EBO')
+
+    # FIXME: Move to apu.py with callbacks?
+    def DumpVoiceBuffer(path):
+      #FIXME: Respect samples per block
+      channels = 2 if is_stereo else 1
+      in_sample_size = container_size_values[container_size]
+      fmt = 0x0011 if container_size == 2 else 0x0001
+      wav = aci.export_wav(path, channels, in_sample_size, freq, fmt)
+
+      samples = ebo + 1
+      if fmt == 0x0011: # Check for ADPCM
+        #FIXME: Is this correct?
+        #FIXME: Rounding issues?
+        block_size =  0x24 * channels
+        bytes = samples // 64 * block_size
+      else:
+        block_size = in_sample_size * channels
+        bytes = samples * block_size
+
+      data = apu.ReadSGE(psl_start_ba, bytes)
+      wav.writeframes(data)
+
+      wav.close()
+    print("Dumping buffer")
+    DumpVoiceBuffer("vp-buffer-" + format(psl_start_ba, '08X') + ".wav")
+
+  next_voice = ReadVoiceField(voice, 'TAR_PITCH_LINK', 'NEXT_VOICE_HANDLE')
+  return next_voice
+
+apu.IterateVoiceLists(ListVoices)
--- python-scripts/xbox/apu.py
+++ python-scripts/xbox/apu.py
@@ -284,197 +284,72 @@ def gp():
   wav.writeframes(data)
   wav.close()
 
+def InvalidateCache(offset = None):
+  pass #FIXME: Auto invalidate on writes near offset with apu functions
 
-def vp(dump_buffers = False):
-  vpsgeaddr = read_u32(NV_PAPU_VPSGEADDR)
-  vpsgeaddr |= 0x80000000
+def RetrievePointer(register):
+  #FIXME: Cache these values!
+  return read_u32(register)
+
+def ReadSSL(offset, length):
+  pass
+
+def ReadSGE(offset, length):
+  vpsgeaddr = RetrievePointer(NV_PAPU_VPSGEADDR)
   def vp_sge(sge_handle):
-    return memory.read_u32(vpsgeaddr + sge_handle * 8)
+    return memory.read_u32(vpsgeaddr + sge_handle * 8, True)
+  out = bytearray()
+  while length > 0:
+    page_base = vp_sge(offset // 0x1000)
+    paged = page_base + offset % 1000
+    in_page = 0x1000 - (offset % 0x1000)
+    # FIXME: Somehow handle this permission stuff differently
+    mapped = map_page(0x80000000 | paged, True)  
+    data = memory.read(paged, min(in_page, length), True)
+    map_page(0x80000000 | paged, mapped)
+    out += data
+    length -= len(data)
+  return bytes(out)
 
-  # Dump voices
+def ReadVoice(voice_handle, field_offset, field_mask):
+  vpvaddr = RetrievePointer(NV_PAPU_VPVADDR) #FIXME: Pass as string so we can look it up in a cache more easily 'NV_PAPU_VPVADDR'
+  value = memory.read_u32(vpvaddr + voice_handle * NV_PAVS_SIZE + field_offset, True); # FIXME: Physical address
+  return GetMasked(value, field_mask)
 
-  vpvaddr = read_u32(NV_PAPU_VPVADDR)
-  vpvaddr |= 0x80000000
-  def dump_voice(voice_handle):
-    def mask(value, field):
-      # FIXME: Also support doing this with just a mask
-      return (value >> field[1]) & ((1 << (field[0] - field[1] + 1)) - 1)
-    
-    voice_addr = vpvaddr + voice_handle * NV_PAVS_SIZE
+def WriteVoice(voice_handle):
+  assert(False) # FIXME
 
-    print("")
-    print("Voice: 0x" + format(voice_handle, '04X'))
+# This dumps information about the active voice, usually not of interest
+if False:
+  NV_PAPU_FEAV_LST_values = ['INHERIT', '2D_TOP', '3D_TOP', 'MP_TOP']
+  feav = read_u32(NV_PAPU_FEAV)
+  active_voice = feav & NV_PAPU_FEAV_VALUE
+  feav_lst = (feav & NV_PAPU_FEAV_LST) >> 16
+  print("Active List: " + str(feav_lst) + " (" + NV_PAPU_FEAV_LST_values[feav_lst] + ")")
 
+# p = 4096*log2(f/48000)
+def PitchToFrequency(pitch):
+  # Get proper sign
+  signed_pitch = int.from_bytes(int.to_bytes(pitch, signed=False, byteorder='little', length=2), byteorder='little', signed=True)
+  return 2 ** (signed_pitch / 4096) * 48000
 
-    vbin = memory.read_u32(voice_addr + NV_PAVS_VOICE_CFG_VBIN)
-    voice_fmt = memory.read_u32(voice_addr + NV_PAVS_VOICE_CFG_FMT)
-    # lots of unk stuff
+# f = 2^(p/4096)*48000
+def FrequencyToPitch(frequency):
+  assert(False) # FIXME
 
-    print("Samples per block: " + str(mask(voice_fmt, NV_PAVS_VOICE_CFG_FMT_SAMPLES_PER_BLOCK)))
-    is_stream = (mask(voice_fmt, NV_PAVS_VOICE_CFG_FMT_DATA_TYPE) > 0)
-    print("Data type: " + ("stream" if is_stream else "buffer"))
+#FIXME: Take a voice_handle as argument instead
+def IterateVoices(addr, callback, name=None):
+  top_voice = read_u32(addr)
+  # print(name + " Top voice: 0x" + format(top_voice, '04X'))
+  next_voice = top_voice
+  while next_voice != 0xFFFF:
+    _next_voice = callback(next_voice, name)
+    if next_voice == _next_voice:
+      assert(False)
+      return
+    next_voice = _next_voice
 
-
-    print("Loop: " + str(mask(voice_fmt, NV_PAVS_VOICE_CFG_FMT_LOOP)))
-
-    # (26,26) ???
-
-
-    is_stereo = mask(voice_fmt, NV_PAVS_VOICE_CFG_FMT_STEREO) > 0
-    print("Stereo: " + str(is_stereo))
-
-    NV_PAVS_VOICE_CFG_FMT_SAMPLE_SIZE_values = [ 'U8', 'S16', 'S24', 'S32' ]
-    sample_size = mask(voice_fmt, NV_PAVS_VOICE_CFG_FMT_SAMPLE_SIZE)
-    print("Sample size: " + NV_PAVS_VOICE_CFG_FMT_SAMPLE_SIZE_values[sample_size])
-
-
-    NV_PAVS_VOICE_CFG_FMT_CONTAINER_SIZE_values = ['B8', 'B16', 'ADPCM', 'B32']
-    container_size_values = [1, 2, 4, 4]
-    container_size = mask(voice_fmt, NV_PAVS_VOICE_CFG_FMT_CONTAINER_SIZE)
-    print("Container size: " + NV_PAVS_VOICE_CFG_FMT_CONTAINER_SIZE_values[container_size])
-    # lots of unk stuff
-
-    psl_start_ba = memory.read_u32(voice_addr + NV_PAVS_VOICE_CUR_PSL_START) & 0xFFFFFF
-    print("buffer start: 0x" + format(psl_start_ba, '08X'))
-
-    cur_psh_sample = memory.read_u32(voice_addr + NV_PAVS_VOICE_CUR_PSH_SAMPLE)
-    print("loop start (samples?): " + format(cur_psh_sample & 0xFFFFFF, '08X'))
-
-    par_state = memory.read_u32(voice_addr + NV_PAVS_VOICE_PAR_STATE)
-    print("State[Paused]: " + str(par_state & NV_PAVS_VOICE_PAR_STATE_PAUSED > 0))
-    print("State[Active]: " + str(par_state & NV_PAVS_VOICE_PAR_STATE_ACTIVE_VOICE > 0))
-
-    par_offset = memory.read_u32(voice_addr + NV_PAVS_VOICE_PAR_OFFSET) # Warning: Upper 8 bits will be 0xFF (?) on hw!
-    print("current offset (samples?): 0x" + format(par_offset & 0xFFFFFF, '08X'))
-
-    ebo = memory.read_u32(voice_addr + NV_PAVS_VOICE_PAR_NEXT) & 0xFFFFFF
-    print("end of buffer (samples): 0x" + format(ebo, '08X'))
-
-    cur_vola = memory.read_u32(voice_addr + NV_PAVS_VOICE_CUR_VOLA)
-    cur_volb = memory.read_u32(voice_addr + NV_PAVS_VOICE_CUR_VOLB)
-    cur_volc = memory.read_u32(voice_addr + NV_PAVS_VOICE_CUR_VOLC)
-
-    tar_vola = memory.read_u32(voice_addr + NV_PAVS_VOICE_TAR_VOLA)
-    tar_volb = memory.read_u32(voice_addr + NV_PAVS_VOICE_TAR_VOLB)
-    tar_volc = memory.read_u32(voice_addr + NV_PAVS_VOICE_TAR_VOLC)
-
-    bins_data = []
-    bins_data.append(vbin & 0x1F) # 0
-    bins_data.append((vbin >> 5) & 0x1F) # 1
-    bins_data.append((vbin >> 10) & 0x1F) # 2
-    # (15,15) ?
-    bins_data.append((vbin >> 16) & 0x1F) # 3
-    bins_data.append((vbin >> 21) & 0x1F) # 4
-    bins_data.append((vbin >> 26) & 0x1F) # 5
-    # (31,31) ?
-    bins_data.append(voice_fmt & 0x1F) # 6
-    bins_data.append((voice_fmt >> 5) & 0x1F) # 7
-
-    def decode_volumes(a, b, c):
-      data = []
-      data.append((a >> 4) & 0xFFF) # 0
-      data.append((a >> 20) & 0xFFF) # 1
-      data.append((b >> 4) & 0xFFF) # 2
-      data.append((b >> 20) & 0xFFF) # 3
-      data.append((c >> 4) & 0xFFF) # 4
-      data.append((c >> 20) & 0xFFF) # 5
-      data.append(((c & 0xF) << 8) | ((b & 0xF) << 4) | (a & 0xF)) # 6
-      data.append(((c >> 8) & 0xF00) | ((b >> 12) & 0xF0) | ((a >> 16) & 0xF)) # 7
-      return data
-    cur_vols_data = decode_volumes(cur_vola, cur_volb, cur_volc)
-    tar_vols_data = decode_volumes(tar_vola, tar_volb, tar_volc)
-
-    #assert(False) #FIXME: Untested!
-
-    bins = ""
-    cur_vols = ""
-    tar_vols = ""
-    for i in range(0, 8):
-      bins += "  " + ("0x" + format(bins_data[i], 'X')).ljust(5)
-      cur_vols += "  0x" + format(cur_vols_data[i], '03X')
-      tar_vols += "  0x" + format(tar_vols_data[i], '03X')
-    print("VBIN   " + bins)
-    print("CUR_VOL" + cur_vols)
-    print("TAR_VOL" + tar_vols)
-
-    tar_pitch_link = memory.read_u32(voice_addr + NV_PAVS_VOICE_TAR_PITCH_LINK)
-    pitch = mask(tar_pitch_link, NV_PAVS_VOICE_TAR_PITCH_LINK_PITCH)
-    next_voice = mask(tar_pitch_link, NV_PAVS_VOICE_TAR_PITCH_LINK_NEXT_VOICE_HANDLE)
-
-    # p=4096*log2(f/48000)
-    # 2^(p/4096)*48000=f
-
-    # Get proper sign
-    signed_pitch = int.from_bytes(int.to_bytes(pitch, signed=False, byteorder='little', length=2), byteorder='little', signed=True)
-    freq = 2 ** (signed_pitch / 4096) * 48000
-
-    print("Pitch: 0x" + format(pitch, '04X') + " (" + str(freq) + "Hz)")
-
-    psl_start_ba_page = psl_start_ba >> 12
-    psl_start_ba_offset = psl_start_ba & 0xFFF
-
-    if dump_buffers and not is_stream: #FIXME: Should only be done while the CPU is paused / no hw is accessing said buffer
-      channels = 2 if is_stereo else 1
-      in_sample_size = container_size_values[container_size]
-      fmt = 0x0011 if container_size == 2 else 0x0001
-      wav = export_wav("buf" + format(psl_start_ba, '08X') + ".wav", channels, in_sample_size, freq, fmt)
-
-      samples = ebo + 1
-      if fmt == 0x0011: # Check for ADPCM
-        #FIXME: Is this correct?
-        #FIXME: Rounding issues?
-        block_size =  0x24 * channels
-        bytes = samples // 64 * block_size
-      else:
-        block_size = in_sample_size * channels
-        bytes = samples * block_size
-
-      while bytes > 0:
-        page_base = vp_sge(psl_start_ba_page)
-        paged = page_base + psl_start_ba_offset
-        in_page = 0x1000 - (psl_start_ba_offset & 0xFFF)
-
-        print("Dumping page " + format(paged, '08X') + " (" + str(bytes) + " bytes left)")
-        paged |= 0x80000000
-        mapped = map_page(paged, True)  
-        data = memory.read(paged, min(in_page, bytes))
-        map_page(paged, mapped)
-
-        bytes -= len(data)
-        wav.writeframes(data)
-
-        psl_start_ba_offset = 0
-        psl_start_ba_page += 1
-      wav.close()
-
-    #Hack to test advancing in buffer:
-    if False:
-      memory.write_u32(voice_addr + NV_PAVS_VOICE_PAR_OFFSET, ebo)
-
-    print("Next voice: 0x" + format(next_voice, '04X'))
-    return next_voice
-
-  # This dumps information about the active voice, usually not of interest
-  if False:
-    NV_PAPU_FEAV_LST_values = ['INHERIT', '2D_TOP', '3D_TOP', 'MP_TOP']
-    feav = read_u32(NV_PAPU_FEAV)
-    active_voice = feav & NV_PAPU_FEAV_VALUE
-    feav_lst = (feav & NV_PAPU_FEAV_LST) >> 16
-    print("Active List: " + str(feav_lst) + " (" + NV_PAPU_FEAV_LST_values[feav_lst] + ")")
-
-  def dump_voices(name, addr):
-    top_voice = read_u32(addr)
-    print(name + " Top voice: 0x" + format(top_voice, '04X'))
-    next_voice = top_voice
-    while next_voice != 0xFFFF:
-      _next_voice = dump_voice(next_voice)
-      if next_voice == _next_voice:
-        assert(False)
-        return
-      next_voice = _next_voice
-
-  dump_voices("2D", NV_PAPU_TVL2D)
-  print("")
-  dump_voices("3D", NV_PAPU_TVL3D)
-  print("")
-  dump_voices("MP", NV_PAPU_TVLMP)
+def IterateVoiceLists(callback):
+  IterateVoices(NV_PAPU_TVL2D, callback, name="2D")
+  IterateVoices(NV_PAPU_TVL3D, callback, name="3D")
+  IterateVoices(NV_PAPU_TVLMP, callback, name="MP")
