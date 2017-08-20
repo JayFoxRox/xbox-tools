782f1a18121e8a552dda8ecc551b3cf15817d901
Jannik Vogel <email@jannikvogel.de>
2017-08-20 22:07:46 +0200

    Parse DVD ROM for Region info

--- dump-dvd-kit/main.py
+++ dump-dvd-kit/main.py
@@ -5,13 +5,11 @@ import math
 import usb1
 import struct
 
-BUFFER_SIZE = 200
-
 with usb1.USBContext() as context:
 
   vid = 0x045e
   pid = 0x0284
-  INTERFACE = 1
+  interface = 1
 
   handle = context.openByVendorIDAndProductID(vid, pid, skip_on_error=True)
   if handle is None:
@@ -19,28 +17,45 @@ with usb1.USBContext() as context:
     print("oops?!")
 
   rom_info = 1
-  info = handle.controlRead(usb1.REQUEST_TYPE_VENDOR | usb1.RECIPIENT_INTERFACE, rom_info, 0, INTERFACE, 6)
+  info = handle.controlRead(usb1.REQUEST_TYPE_VENDOR | usb1.RECIPIENT_INTERFACE, rom_info, 0, interface, 6)
 
   (version, code_length) = struct.unpack("<HI", info)
-  print("version %X.%X" % (version >> 8, version & 0xFF))
-  print(str(code_length - 6) + " bytes")
+  print("Version: %X.%X" % (version >> 8, version & 0xFF))
+  print("Size: " + str(code_length) + " bytes")
 
-  with open('default.xbe', 'wb') as f:
+  with open("dvd-dongle-rom.bin", 'wb') as f:
 
     rom_download = 2
     remaining = code_length
     cursor = 0
+
+    xbe = bytes([])
+
     while(remaining > 0):
       chunkSize = min(remaining, 1024)
-      data = handle.controlRead(usb1.REQUEST_TYPE_VENDOR | usb1.RECIPIENT_INTERFACE, rom_download, cursor >> 10, INTERFACE, chunkSize)
+      data = handle.controlRead(usb1.REQUEST_TYPE_VENDOR | usb1.RECIPIENT_INTERFACE, rom_download, cursor >> 10, interface, chunkSize)
       assert(chunkSize == len(data))
       # The first block contains a copy of the (version, code_length)
       if cursor == 0:
         assert(data[0:6] == info)
-        data = data[6:]
+        xbe += data[6:]
+      else:
+        xbe += data
       f.write(data)
       remaining -= chunkSize
       cursor += chunkSize
 
+  # Do some sanity checks and print out DVD region
 
-
+  BaseAddress = struct.unpack('I', xbe[260:264])[0]
+  Certificate = struct.unpack('I', xbe[280:284])[0] - BaseAddress
+  SectionHeaders = struct.unpack('I', xbe[288:292])[0] - BaseAddress
+  SizeOfImage = struct.unpack('I', xbe[268:272])[0]
+  assert((6 + SizeOfImage) == code_length) # SizeOfImage
+  assert(struct.unpack('I', xbe[Certificate+156:Certificate+160])[0] == 0x00000100) # AllowedMediaTypes == DONGLE
+  assert(struct.unpack('I', xbe[Certificate+172:Certificate+176])[0] == version) # Version
+  RawData = struct.unpack('I', xbe[SectionHeaders+12:SectionHeaders+16])[0]
+  SizeOfRawData = struct.unpack('I', xbe[SectionHeaders+16:SectionHeaders+20])[0]
+  assert(RawData + SizeOfRawData == SizeOfImage)
+  GameRegion = struct.unpack('I', xbe[Certificate+160:Certificate+164])[0]
+  print("Region: " + str(GameRegion))
